ea0218f38fcf708cd677d8ce2519938430a5bb7b
StuartOG <otisstu1@gmail.com>
2023-09-06 18:57:59 +0100

    Create Division.py

---
+++ ACS-Tasks/Division.py
@@ -0,0 +1,6 @@
+#program to divide two integers
+from math import *
+integer_one = int(input("Please enter an integer: "))
+integer_two = int(input("Please enter a second integer: "))
+answer = integer_one/integer_two
+print("Your answer is", answer)
