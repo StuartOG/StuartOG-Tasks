a87e2c491e2cbea71fe44552ddec593c18f27586
StuartOG <otisstu1@gmail.com>
2023-09-07 09:16:20 +0100

    Number Output updated

---
+++ ACS-Tasks/Hundred's.py
@@ -0,0 +1,6 @@
+#program to display the hendred's tens and units in a number
+n=int(input("Give an integer between 100 and 999: "))
+s = str(n)
+for i in s:
+    print(i)
+
---
+++ ACS-Tasks/NumberOutput.py
@@ -0,0 +1,5 @@
+#program to output thee first 10 integers
+counter = 1
+while counter <= 10:
+    print(counter)
+    counter += 1
