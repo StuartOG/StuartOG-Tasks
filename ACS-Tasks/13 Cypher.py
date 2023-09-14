781b00454359d628bb3b78a549a9f8e21dedbe1c
StuartOG <otisstu1@gmail.com>
2023-09-14 16:46:58 +0100

    update

---
+++ ACS-Tasks/13 Cypher.py
@@ -0,0 +1,33 @@
+
+
+
+def caeser_cypher(sentence, shift):
+    cypher = ""
+
+
+    for char in sentence:
+        if char.isalpha():
+        
+            char_shift = chr((ord(char.lower) - ord('a') + shift % 26) + ord('a'))
+            cypher += char_shift
+        
+        else:
+        
+            cypher += char
+
+    return cypher
+
+sentence = input("Enter a sentence: ")
+
+
+
+shift = int(input("Give a number to shift by: "))
+
+cypher = caeser_cypher(sentence, shift)
+print(cypher)
+
+
+
+
+
+
