b5b04462d25c59303f4eb5a3aef00ac41bded58a
StuartOG <otisstu1@gmail.com>
2023-09-27 10:10:50 +0100

    update

--- ACS-Tasks/15 Bowling.py
+++ ACS-Tasks/15 Bowling.py
@@ -1,15 +1,50 @@
+bowling_score = []
 
-counter = 1
-while counter <= 10:
-    score = int(input("Inpiut your first score"))
-    if score == 10:
-        print("You scored a strike")
-        counter += 1
-    else:
-        score_2 = int(input("Inpiut your second score"))
-        score_3 = score_2 + score
-        if score_3 == 10:
-            print("You scored a spare")
-            counter += 1
+def bowling():
+    while turns < 10:
+        score = int(input("Input your first score"))
+        if score == 10:
+            print("You scored a strike")
+            turns += 1
+            strike()
         else:
-            print("You scored", score_3)
+            score_2 = int(input("Input your second score"))
+            score_3 = score_2 + score
+            if score_3 == 10:
+                print("You scored a spare")
+                bowling_score.append[10]
+                bowling()
+            else:
+                print("You scored", score_3)
+                turns += 1
+                bowling_score.append[score_3]
+                bowling()
+
+def last_turn():
+    if turns == 10:
+        score = int(input("Input your first score"))
+        if score == 10:
+            print("You scored a strike")
+            turns += 1
+            strike()
+        else:
+            score_2 = int(input("Input your second score"))
+            score_3 = score_2 + score
+            if score_3 == 10:
+                print("You scored a spare")
+                bowling_score.append[10]
+                bowling()
+            else:
+                print("You scored", score_3)
+                turns += 1
+                bowling_score.append[score_3]
+                bowling()
+
+
+    
+def strike():
+    int(input("Input your next score"))
+
+print(bowling_score)
+    
+
--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-def prco1(x, y):
-    x = [1, 2, 3, 4]
-    y = 0
 
-m = [1, 2, 4]
-n = 10
-prco1(m, n)
-print(m, n)
