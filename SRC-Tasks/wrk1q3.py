568b23a8fa86730c6971c6ead98fba5e0dce4fe6
StuartOG <otisstu1@gmail.com>
2023-10-06 15:17:12 +0100

    Create wrk1q3.py

---
+++ SRC-Tasks/wrk1q3.py
@@ -0,0 +1,8 @@
+Outlet1Sales = [10, 12,15,10]
+Outlet2Sales = [5,8,3,6]
+Outlet3Sales = [10,12,15,10]
+
+for qtr in range(4):
+    total=Outlet1Sales[qtr]+Outlet2Sales[qtr]+Outlet3Sales[qtr]
+    print("Totale for quarter = ", total)
+#next qtr
