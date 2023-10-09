4308dd79064a003e8d3c46387354f2d5edf9b596
StuartOG <otisstu1@gmail.com>
2023-10-09 16:29:09 +0100

    update

---
+++ SRC-Tasks/2darrays.py
@@ -0,0 +1,18 @@
+rows, cols = (6, 4)
+arr = [['x' for i in range(cols)] for j in range(rows)]
+arr[0][0]='O'
+for row in arr:
+    for x in row:
+        print(x, end=' ')
+    print()
+my_row = int(input("Enter a row to move to: "))
+my_row = my_row - 1
+my_col = int(input("Enter a col to move to: "))
+my_col = my_col - 1
+
+arr[0][0] = 'X'
+arr[my_row, my_col] = 'O'
+for row in arr:
+    for x in row:
+        print(x, end=' ')
+    print()
--- SRC-Tasks/wrk1q6.py
+++ SRC-Tasks/wrk1q6.py
@@ -1,24 +1,2 @@
-x = "empty"
-y = "taken"
-car_park_array = [
-    [x,x,x,x,x,x,x,x,x,x],
-    [x,x,x,x,x,x,x,x,x,x],
-    [x,x,x,x,x,x,x,x,x,x],
-    [x,x,x,x,x,x,x,x,x,x],
-    [x,x,x,x,x,x,x,x,x,x],
-    [x,x,x,x,x,x,x,x,x,x],
-]
-for col in car_park_array:
-    print(col)
-parking_row = input("Input row you parked: ")
-parking_col = input("Input column you parked: ")
-car_park_array[parking_col][parking_row] = y
-for i in car_park_array:
-    if parking_row and parking_col == x:
-        print("Valid parking spot")
-        car_park_array[parking_col][parking_row] = y
-    elif parking_row and parking_row == y:
-        print("Invalid spot")
-for col in car_park_array:
-    print(col)
+
 
