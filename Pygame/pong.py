2e99e4abaec38723e788b667df1d85cde48ed6b4
StuartOG <otisstu1@gmail.com>
2023-10-10 15:44:35 +0100

    Update pong.py

--- Pygame/pong.py
+++ Pygame/pong.py
@@ -15,9 +15,28 @@ BLUE     = (   0,   0, 255)
 YELLOW   = (255 , 255,   0)
 PURPLE   = ( 150,   0, 150)
 PINK     = ( 255, 192, 203)
+blue_green = ((0,255,170))
+marroon = ((115,0,0))
+lime = ((180,255,100))
+gray = ((127,127,127))
+magenta = ((255,0,230))
+brown = ((100,40,0))
+forest_green = ((0,50,0))
+navy_blue = ((0,0,100))
+rust = ((210,150,75))
+dandilion_yellow = ((255,200,0))
+highlighter = ((255,255,100))
+sky_blue = ((0,255,255))
+light_gray = ((200,200,200))
+dark_gray = ((50,50,50))
+tan = ((230,220,170))
+coffee_brown =((200,190,140))
+moon_glow = ((235,245,255))
+colours = [GREEN,RED,BLUE,PURPLE,PINK, blue_green, marroon, lime,gray,magenta,brown,forest_green,navy_blue,rust,dandilion_yellow,highlighter
+           ,sky_blue,light_gray,dark_gray,tan,coffee_brown,moon_glow]
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Otis's test:")
+pygame.display.set_caption("Otis's pong game")
 
 # Loop until the user clicks the close button.
 done = False
@@ -30,16 +49,19 @@ x_offset = 5
 y_offset = 5
 pi= 3.141592652
 counter = 0
-screenfill = BLUE
+screenfill_fill = BLUE
 randomint = random.randint(1,4)
 y_val_2 = 200
 x_val_2 = 5
+y_val_3 = 200
 lives = 5
 text_font = pygame.font.SysFont(None, 30)
-font = pygame.font.SysFont(None, 30)
+font = pygame.font.SysFont(None, 50)
 end = ""
+right_lives = 0
+left_lives = 0
 
- 
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -55,20 +77,27 @@ while not done:
     # above this, or they will be erased with this command.
    
     
-    screen.fill(screenfill)
+    screen.fill(screenfill_fill)
     
     
     #draw stuff here:
     endmessage = font.render(end, True, BLACK)
     screen.blit(endmessage, [270,100])
-    lifecount=font.render('your life count is ' + str(lives), True, BLACK)
-    screen.blit(lifecount, [250, 100])
+    player_two_lifecount=font.render(str(left_lives), True, BLACK)
+    screen.blit(player_two_lifecount, [100, 100])
+    player_one_lifecount=font.render(str(right_lives), True, BLACK)
+    screen.blit(player_one_lifecount, [600, 100])
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         y_val_2 -= 5
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_s]:
         y_val_2 += 5
+
+    if keys[pygame.K_UP]:
+        y_val_3 -= 5
+    if keys[pygame.K_DOWN]:
+        y_val_3 += 5
 
 
     y_val = y_val+ y_offset
@@ -81,42 +110,68 @@ while not done:
         y_offset = y_offset*-1 
         
     
-    elif x_val > 695:
-        x_offset = x_offset*-1 
-    
     elif x_val < 20 and y_val >= y_val_2 and y_val <= y_val_2 + 100:
         x_offset = x_offset*-1 
-        y_val = y_val+ y_offset+1
-        x_val = x_val+ y_offset+1
+        screenfill_fill=random.choice(colours)
+
+    elif x_val > 665 and y_val >= y_val_3 and y_val <= y_val_3 + 100:
+        x_offset = x_offset*-1 
+        screenfill_fill=random.choice(colours)
         
     if x_val < 0 and y_val >= y_val_2+100:
         screenfill = RED
         x_val = 350
         y_val = 250
         screenfill = BLUE
-        lives -= 1
+        right_lives+= 1
     
     elif x_val < 0 and y_val <= y_val_2:
         screenfill = RED
         x_val = 350
         y_val = 250
         screenfill = BLUE
-        lives -= 1    
+        right_lives += 1 
     
-    if lives == 0:
-        end = "GAME OVER"
+    if x_val > 700 and y_val >= y_val_3+100:
+        screenfill = RED
+        x_val = 350
+        y_val = 250
+        screenfill = BLUE
+        left_lives += 1
+
+    elif x_val > 700 and y_val <= y_val_3:
+        screenfill = RED
+        x_val = 350
+        y_val = 250
+        screenfill = BLUE
+        left_lives += 1    
+       
+    
+    if left_lives > 5:
+        end = "Player One Wins"
         endmessage = font.render(end, True, BLACK)
-        screen.blit(endmessage, [270,75])
+        screen.blit(endmessage, [250,75])
+        pygame.display.flip()
+        time.sleep(1)
+        done = True
+    
+    if right_lives > 5:
+        end = "Player Two Wins"
+        endmessage = font.render(end, True, BLACK)
+        screen.blit(endmessage, [250,75])
         pygame.display.flip()
         time.sleep(1)
         done = True
     
 
     #end if
+    pygame.draw.rect(screen, BLACK, (x_val, y_val, 12, 12))
 
-    pygame.draw.circle(screen, YELLOW, [x_val, y_val] , 10)
+    pygame.draw.rect(screen, WHITE, (x_val, y_val , 10, 10))
 
-    pygame.draw.rect(screen, WHITE, (5, y_val_2, 15, 100))
+    pygame.draw.rect(screen, BLACK, (5, y_val_2, 15, 100))
+
+    pygame.draw.rect(screen, BLACK, (680, y_val_3, 15, 100 ))
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
