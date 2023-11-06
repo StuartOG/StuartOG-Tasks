9edff8b0c2bb78ef5d716648b9e5a5ce82b40dd9
StuartOG <otisstu1@gmail.com>
2023-11-06 12:41:13 +0000

    Update queue2.py

--- SRC-Tasks/queue2.py
+++ SRC-Tasks/queue2.py
@@ -15,6 +15,43 @@ class Queue_data:
         #end while
         return return_str
     #end function
+    def isEmpty(self):
+        return self.size == 0
+
+    def enqueue(self,item):
+        if not isFull(self):
+            self.rp = (self.rp + 1) % self.max_size
+            self.size += 1
+            self.data[self.rp] = item
+        else:
+            print("queue full")
+    #end if
+    def isEmpty(self):
+        return self.size == 0
+    #end function
+    def isFull(self):
+        return self.max_size == self.size
+    #end function
+    def enqueue(self,item):
+        if not self.isFull():
+            self.rp = (self.rp + 1) % self.max_size
+            self.size += 1
+            self.data[self.rp] = item
+        else:
+            print("queue full")
+        #end if
+    #end procedure
+    def dequeue(self):
+        if not self.isEmpty():
+            item_p = self.fp
+            self.fp = (self.fp + 1) % self.max_size
+            self.size -= 1
+            return self.data[item_p]
+        else:
+            print("Queue empty")
+        #end if
+#end function
+#end procedure
             
 #end record
 def isEmpty(q):
