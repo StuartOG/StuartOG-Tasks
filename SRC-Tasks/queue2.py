9d7b080b5b8fabf1c5fb039412fc07b73f9d225c
StuartOG <otisstu1@gmail.com>
2023-11-07 09:27:34 +0000

    Update queue2.py

--- SRC-Tasks/queue2.py
+++ SRC-Tasks/queue2.py
@@ -1,52 +1,34 @@
-class Queue_data:
-    def __init__(self,max_size):
-        self.max_size = max_size
-        self.data = [0 for _ in range(self.max_size)]
-        self.size = 0
+class Queue:
+    def __init__(self):
+        self.data = []
         self.fp = 0
         self.rp = -1
     #end constructor
-    def __repr__(self) -> str:
-        return_str = ""
-        ptr = self.fp
-        while ptr != (self.rp + 1) % self.max_size: 
-            return_str += str(self.data[ptr]) + " "
-            ptr = (ptr + 1) % self.max_size
-        #end while
-        return return_str
-    #end function
+
+    # def __repr__(self) -> str:
+    #     return_str = ""
+    #     ptr = self.fp
+    #     while ptr != (self.rp + 1) % self.max_size: 
+    #         return_str += str(self.data[ptr]) + " "
+    #         ptr = (ptr + 1) % self.max_size
+    #     #end while
+    #     return return_str
+    # #end function
     def isEmpty(self):
-        return self.size == 0
+        return len(self.data) == 0
 
     def enqueue(self,item):
-        if not isFull(self):
-            self.rp = (self.rp + 1) % self.max_size
-            self.size += 1
-            self.data[self.rp] = item
-        else:
-            print("queue full")
-    #end if
+        self.data.append(item)
+           
+    #end function
     def isEmpty(self):
         return self.size == 0
     #end function
-    def isFull(self):
-        return self.max_size == self.size
-    #end function
-    def enqueue(self,item):
-        if not self.isFull():
-            self.rp = (self.rp + 1) % self.max_size
-            self.size += 1
-            self.data[self.rp] = item
-        else:
-            print("queue full")
         #end if
     #end procedure
     def dequeue(self):
         if not self.isEmpty():
-            item_p = self.fp
-            self.fp = (self.fp + 1) % self.max_size
-            self.size -= 1
-            return self.data[item_p]
+            self.data.pop(0)
         else:
             print("Queue empty")
         #end if
@@ -54,61 +36,37 @@ class Queue_data:
 #end procedure
             
 #end record
-def isEmpty(q):
-    return q.size == 0
-#end function
-def isFull(q):
-    return q.max_size == q.size
-#end function
-def enqueue(item,q):
-    if not isFull(q):
-        q.rp = (q.rp + 1) % q.max_size
-        q.size += 1
-        q.data[q.rp] = item
-    else:
-        print("queue full")
-    #end if
-#end procedure
-def dequeue(q):
-    if not isEmpty(q):
-        item_p = q.fp
-        q.fp = (q.fp + 1) % q.max_size
-        q.size -= 1
-        return q.data[item_p]
-    else:
-        print("Queue empty")
-    #end if
-#end function
-new_q1 = Queue_data(5)
-new_q2 = Queue_data(7)
+
+new_q1 = Queue()
+new_q2 = Queue()
 print(new_q1.data)
 print(new_q2.data)
 for num in range(11,15):
-    enqueue(num,new_q1)
+    new_q1.enqueue(num)
 #next num
 for num in range(101,106):
-    enqueue(num,new_q2)
+    new_q2.enqueue(num)
 #next num
 print(new_q1)
 print(new_q1.data)
 print(new_q2.data)
-enqueue(15,new_q1)
-enqueue(16,new_q1)
-enqueue(150,new_q1)
-enqueue(160,new_q2)
+new_q1.enqueue(15)
+new_q1.enqueue(16)
+new_q1.enqueue(150)
+new_q2.enqueue(160)
 print(new_q1.data)
 print(new_q2.data)
 for _ in range(6):
-    print(dequeue(new_q1))
+    print(new_q1.dequeue())
 #next _
 for _ in range(8):
-    print(dequeue(new_q2))
+    print(new_q2.dequeue())
 #next _
 print(new_q1.data)
 print(new_q2.data)
-enqueue(20,new_q1)
-enqueue(200,new_q2)
+new_q1.enqueue(20)
+new_q2.enqueue(200)
 print(new_q1.data)
 print(new_q2.data)
-print(new_q1)
+#print(new_q1)
         
