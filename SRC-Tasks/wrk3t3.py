fae0ccb8a4304391fffa3f1752861ade32d50957
StuartOG <otisstu1@gmail.com>
2023-11-17 15:24:31 +0000

    Update wrk3t3.py

--- SRC-Tasks/wrk3t3.py
+++ SRC-Tasks/wrk3t3.py
@@ -1,38 +1,47 @@
 class Node:
-    def __init__(self,name,pointer) -> None:
+    def __init__(self, name , pointer):
         self.name = name
         self.pointer = pointer
     #end constructor
+
     def __repr__(self) -> str:
-        return "Data:"+self.name+",Ptr:"+str(self.pointer)
-#end Node record
-# Create array of blank Nodes (records)
-myList = [Node("",-1) for _ in range(5) ]
-for index in range(4):
+        return " |Data: "  + self.name + "   Ptr: " + str(self.pointer) + "| "
+    
+
+    def orderOutput(self):
+        print(self)
+#end node record
+
+myList = [Node("Empty", -1) for _ in range(5)]
+
+for index in range(0,4):
     myList[index].pointer = index + 1
 #next index
 myList[4].pointer = -1
-start_pointer = 1
-nextfree = -1
-print(myList)
 
-def outputList(arr):
-    global start_pointer
-    current_pointer = start_pointer
-    while current_pointer != -1:
-        print(arr[current_pointer].name)
-        current_pointer=arr[current_pointer].pointer
-    #end while
-#end procedure
 
-def AddItem(newItem, myList):
+start = -1
+nextfree = 0
+
+def printOrder(Mylinkedlist):
+    global start
+    Current_pointer = start
+    while Current_pointer != -1:
+        print(Mylinkedlist[Current_pointer].name)
+        Current_pointer = Mylinkedlist[Current_pointer].pointer
+    #next current_pointer
+#printOrder(linked_list_data)
+
+
+def AddItem(newName):
     global nextfree
     global start
     #check if list is full and if so, print error message
     if nextfree == -1:
         print("Error")
     else:
-        newItem = myList[nextfree].name
+        myList[nextfree].name = newName
+
         if start == -1:
             temp = myList[nextfree].pointer       #save pointer
             myList[nextfree].pointer = -1
@@ -40,29 +49,33 @@ def AddItem(newItem, myList):
             nextfree = temp
         else:
             p = start
-            if newItem < myList[p].name:  
+            if newName < myList[p].name:  
                 start  = myList[nextfree].pointer
                 start = nextfree
             else:   
                 placeFound = False    #general case
                 while myList[p].pointer != -1 and placeFound == False:
                     #peek ahead
-                    if newItem >= myList[myList[p].pointer].name:
+                    if newName >= myList[myList[p].pointer].name:
                         p = myList[p].pointer
                     else:
-                        placefound = True
+                        placeFound = True
                     
                 
                 temp = nextfree
                 nextfree = myList[nextfree].pointer
                 myList[temp].pointer = myList[p].pointer
                 myList[p].pointer = temp
-
-AddItem("Colin",myList)
 print(myList)
-# AddItem("Albert",myList)
-# AddItem("Barry",myList)
-# AddItem("Derek",myList)
-# AddItem("Fred",myList)
-# outputList(myList)
-# AddItem("Trevor",myList)
+# printOrder(myList1)
+AddItem("Colin")
+# AddItem("Albert",myList1)
+# AddItem("Barry",myList1)
+print( "----------------")
+print(myList)
+
+# printOrder(myList1)
+# AddItem("Derek",myList1)
+# AddItem("Fred",myList1)
+# #outputList(myList)
+# AddItem("Trevor",myList1)
