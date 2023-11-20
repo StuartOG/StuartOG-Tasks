fdb79787ef7f9da84879d1dc7acc6fb2ceeed3d2
StuartOG <otisstu1@gmail.com>
2023-11-20 09:00:13 +0000

    Update wrk3t3.py

--- SRC-Tasks/wrk3t3.py
+++ SRC-Tasks/wrk3t3.py
@@ -33,18 +33,18 @@ def AddItem(newItem, myList):
         print("Error")
     else:
         myList[nextfree].name = newItem
-        if start == -1:
+        if start == 0:
             temp = myList[nextfree].pointer       #save pointer
             myList[nextfree].pointer = -1
             start = nextfree
             nextfree = temp
         else:
             p = start
-            if newItem < myList[p].name:
+            if myList[nextfree].name < myList[p].name:
                 temp = myList[nextfree].pointer  
-                start = myList[nextfree].pointer
-                start = nextfree
+                myList[nextfree].pointer = start
                 nextfree = temp
+                myList[p].pointer = nextfree
             else:   
                 placefound = False    #general case
                 while myList[p].pointer != -1 and placefound == False:
@@ -63,12 +63,31 @@ def AddItem(newItem, myList):
                 myList[temp].pointer = myList[p].pointer
                 myList[p].pointer = temp
 
+def removeItem(Item):
+    global nextfree
+    global start
+    if start == -1:
+        print("list is empty")
+    else:
+        p = start
+        if Item == myList[start].name:
+            start = myList[start].pointer
+        else:
+            while Item != myList[myList[p].pointer].name:
+                p = myList[p].pointer
+            #endwhile
+        #endif
+    #endif
+    nextfree = myList[p].pointer
+    myList[p].pointer = myList[nextfree].pointer
+
 
 AddItem("Colin",myList)
 AddItem("Albert",myList)
-AddItem("Barry",myList)
-AddItem("Derek",myList)
-AddItem("Fred",myList)
+# AddItem("Barry",myList)
+# AddItem("Derek",myList)
+# AddItem("Fred",myList)
+print(myList)
 print(nextfree)
 # outputList(myList)
 # AddItem("Trevor",myList)
